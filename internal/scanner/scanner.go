ae02c3b10032534138bbac033a41e27522ff2857
darkvadr <glowburn.acc@gmail.com>
2026-01-17 06:56:08 +0000

    feat: enhance scanner with directory walking and target directory identification

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -2,6 +2,9 @@ package scanner
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
+	"path/filepath"
 	"sync"
 	"time"
 
@@ -27,6 +30,16 @@ func NewScanner(cfg *models.Config, cache CacheProvider) *Scanner {
 		results: make(chan models.DependencyFolder),
 		errors:  make(chan error),
 	}
+}
+
+var targetDirectories = []string{
+	"node_modules",       // common Node.js dependencies folder
+	"node_modules_cache", // alternative Node.js cache folder
+	"vendor",             // Go/Php vendor folders
+	".venv",              // Python virtual environment
+	"__pycache__",        // Python cache folder
+	"venv",               // Python virtual environment
+	"target",             // Rust target folder
 }
 
 // Scan initiates file traversal process
@@ -76,6 +89,73 @@ func (s *Scanner) worker(ctx context.Context, wg *sync.WaitGroup) {
 
 }
 
-func (s *Scanner) walkFileSystem(ctx context.Context, rootPath string, depth int) {
-	// File system walking logic
+func (s *Scanner) isTargetDirectory(name string) bool {
+
+	for _, target := range targetDirectories {
+		if name == target {
+			return true
+		}
+	}
+
+	return false
+
+}
+
+func (s *Scanner) walkFileSystem(ctx context.Context, rootPath string, depth int) error {
+
+	pathDepths := make(map[string]int)
+	pathDepths[rootPath] = depth
+	// WalkDir is a convenient way to traverse directories tho
+	// it may need to be replaced with a custom implementation
+	// if we need to follow symlinks
+	return filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
+
+		// check for context cancellation
+		select {
+		case <-ctx.Done():
+			return fs.SkipAll
+		default:
+			// continue processing
+		}
+
+		if err != nil {
+			s.errors <- fmt.Errorf("accessing %s: %w", path, err)
+			return fs.SkipDir // skip this directory on error but keep walking
+		}
+
+		if !d.IsDir() {
+			return nil // we only care about directories
+		}
+
+		parentDepth := pathDepths[filepath.Dir(path)]
+		currentDepth := parentDepth + 1
+		pathDepths[path] = currentDepth
+
+		// check max depth
+		if s.config.MaxDepth > 0 && currentDepth > s.config.MaxDepth {
+			return fs.SkipDir
+		}
+
+		// TODO: check ignore paths
+
+		if s.isTargetDirectory(d.Name()) {
+
+			// info, _ := d.Info()
+
+			s.enqueueAndAnalysis(path)
+
+			return fs.SkipDir // skip further traversal into this directory
+
+		}
+
+		return nil
+
+	})
+
+}
+
+func (s *Scanner) enqueueAndAnalysis(path string) {
+
+	// send directory for analysis
+
 }
